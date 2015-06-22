76f516b1251a2f0d0f986143183c6c1d8daf131a
Anonim47 <yosefadis@hotmail.com>
2015-06-22 08:12:21 -0400

    Initial support for lenovo armani
    Signed-off-by: Anonim47 <yosefadis@hotmail.com>

---
+++ cm.mk
@@ -0,0 +1,18 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 854
+TARGET_SCREEN_HEIGHT := 480
+
+# Release name
+PRODUCT_RELEASE_NAME := armani
+PRODUCT_NAME := cm_armani
+
+$(call inherit-product, device/lenovo/armani/full_armani.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_BRAND=LENOVO \
+    PRODUCT_NAME=armani \
+    BUILD_PRODUCT=A706_ROW
+
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo cm_armani-userdebug
+sh device/lenovo/armani/patches/apply.sh
