77651b9943ce7039162779ae1529289a1883a4de
Anonim47 <yosefadis@hotmail.com>
2015-06-30 12:10:15 -0400

    armani: Deltadroidy
    Signed-off-by: Anonim47 <yosefadis@hotmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "DNS S4503"
+#define BTM_DEF_LOCAL_NAME   "DeltaDroid-A706"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
